c98216d61b4c6ab8d0a7068d8d6a98c007ae410b
AlexTicovschi <alexmticovschi@gmail.com>
2020-02-26 20:47:02 +0000

    using routes and controllers

---
+++ backend/routes/blog.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const { time } = require('../controllers/blog');
+
+router.get('/blogs', time);
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const blogRoutes = require('./routes/blog');
+
 // app
 const app = express();
 
@@ -29,10 +31,8 @@ if (process.env.NODE_ENV === 'development') {
   app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
 }
 
-// routes
-app.get('/api', (req, res) => {
-  res.json({ time: Date().toString() });
-});
+// routes middleware
+app.use('/api', blogRoutes);
 
 // port
 const port = process.env.PORT || 8000;
