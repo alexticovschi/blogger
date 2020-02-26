7ce4ce76e666664af8626654607ee25dbe549f73
AlexTicovschi <alexmticovschi@gmail.com>
2020-02-26 19:27:48 +0000

    connecting to mongodb

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,21 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 // app
 const app = express();
+
+// db
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connected'));
 
 // middlewares
 app.use(morgan('dev'));
