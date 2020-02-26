57e8ad352f551a4d3ba63de503973c7b8b13b2f2
AlexTicovschi <alexmticovschi@gmail.com>
2020-02-26 17:54:41 +0000

    setting up the server

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+// app
+const app = express();
+
+// middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// cors
+app.use(cors());
+
+// routes
+app.get('/api', (req, res) => {
+  res.json({ time: Date().toString() });
+});
+
+// port
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Up and running on port: ${port}`));
