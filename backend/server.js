37e677e3cc7c5b69029aba82849ef9ee6d8b5685
AlexTicovschi <alexmticovschi@gmail.com>
2020-02-26 19:13:35 +0000

    configuring cors

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,9 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 // cors
-app.use(cors());
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+}
 
 // routes
 app.get('/api', (req, res) => {
