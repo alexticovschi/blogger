66120602f62924a2e0261bf25833fd2c54cd2662
AlexTicovschi <alexmticovschi@gmail.com>
2020-02-27 20:33:31 +0000

    signing users out and protecting  routes

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -60,3 +60,14 @@ exports.signin = (req, res) => {
     });
   });
 };
+
+exports.signout = (req, res) => {
+  res.clearCookie('token');
+  res.json({
+    message: 'Signout success'
+  });
+};
+
+exports.requireSignin = expressJwt({
+  secret: process.env.JWT_SECRET
+});
--- backend/routes/auth-routes.js
+++ backend/routes/auth-routes.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { signup, signin } = require('../controllers/auth-controller');
+const {
+  signup,
+  signin,
+  signout,
+  requireSignin
+} = require('../controllers/auth-controller');
 
 // validators
 const { runValidation } = require('../validators');
@@ -12,5 +17,13 @@ const {
 // if validation is passed, execute the code in signup controller
 router.post('/signup', userSignupValidator, runValidation, signup);
 router.post('/signin', userSigninValidator, runValidation, signin);
+router.get('/signout', signout);
+
+// test
+router.get('/secret', requireSignin, (req, res) => {
+  res.json({
+    message: 'you have access to secret page'
+  });
+});
 
 module.exports = router;
