6a4abd55f46def8a1d1436a5167a2b41593d1324
AlexTicovschi <alexmticovschi@gmail.com>
2020-02-27 07:24:01 +0000

    creating a signup validator

--- backend/routes/auth-routes.js
+++ backend/routes/auth-routes.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { signup } = require('../controllers/auth-controller');
 
-router.post('/signup', signup);
+// validators
+const { runValidation } = require('../validators');
+const { userSignupValidator } = require('../validators/auth-validator');
+
+// if validation is passed, execute the code in signup controller
+router.post('/signup', userSignupValidator, runValidation, signup);
 
 module.exports = router;
