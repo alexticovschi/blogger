0bb2cd591b43f47320e9b4d3fbe5a9308ee5e729
AlexTicovschi <alexmticovschi@gmail.com>
2020-02-28 09:18:09 +0000

    cleaning the Header code

--- frontend/components/Header/Header.js
+++ frontend/components/Header/Header.js
@@ -7,12 +7,7 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText
+  NavLink
 } from 'reactstrap';
 
 const Header = () => {
