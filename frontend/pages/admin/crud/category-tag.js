db02182c6a3478d98499de0ae07a69ee7291943e
AlexTicovschi <alexmticovschi@gmail.com>
2020-03-07 15:27:28 +0000

    building the category page

---
+++ frontend/pages/admin/crud/category-tag.js
@@ -0,0 +1,25 @@
+import Layout from '../../../components/Layout';
+import Admin from '../../../components/auth/Admin/Admin';
+import Link from 'next/link';
+
+const CategoryTag = () => (
+  <Layout>
+    <Admin>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-12 py-5'>
+            <h4>Manage Categories and Tags</h4>
+          </div>
+          <div className='col-md-6'>
+            <h5>Categories</h5>
+          </div>
+          <div className='col-md-6'>
+            <h5>Tag</h5>
+          </div>
+        </div>
+      </div>
+    </Admin>
+  </Layout>
+);
+
+export default CategoryTag;
--- frontend/pages/admin/index.js
+++ frontend/pages/admin/index.js
@@ -1,10 +1,27 @@
 import Layout from '../../components/Layout';
 import Admin from '../../components/auth/Admin/Admin';
+import Link from 'next/link';
 
 const AdminPage = () => (
   <Layout>
     <Admin>
-      <h2>Admin Dashboard</h2>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-12 py-5'>
+            <h4>Admin Dashboard</h4>
+          </div>
+          <div className='col-md-4'>
+            <ul className='list-group'>
+              <li className='list-group-item'>
+                <Link href='/admin/crud/category-tag'>
+                  <a>Create Category</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className='col-md-8'>right</div>
+        </div>
+      </div>
     </Admin>
   </Layout>
 );
