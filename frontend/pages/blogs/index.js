3ca9fbd6e7ef83ff6eb4e04fbf689d4e5546c0eb
AlexTicovschi <alexmticovschi@gmail.com>
2020-03-24 11:24:35 +0000

    displaying all categories and tags on Blogs page

--- frontend/pages/blogs/index.js
+++ frontend/pages/blogs/index.js
@@ -3,6 +3,7 @@ import Layout from '../../components/Layout';
 import Card from '../../components/blog/Card/Card';
 import { useState } from 'react';
 import { fetchBlogsWithCategoriesAndTags } from '../../actions/blog';
+import Link from 'next/link';
 
 const BlogsPage = ({ blogs, categories, tags, size }) => {
   return (
@@ -14,6 +15,25 @@ const BlogsPage = ({ blogs, categories, tags, size }) => {
               <h1 className='display-5 font-weight-bold text-center'>
                 Programming blogs and tutorials
               </h1>
+
+              <section className='text-center'>
+                {categories.map(category => (
+                  <Link
+                    key={category._id}
+                    href={`/categories/${category.slug}`}
+                  >
+                    <a className='btn btn-outline-info mr-2 mt-1'>
+                      {category.name}
+                    </a>
+                  </Link>
+                ))}
+
+                {tags.map(tag => (
+                  <Link key={tag._id} href={`/tags/${tag.slug}`}>
+                    <a className='btn btn-outline-dark mr-2 mt-1'>{tag.name}</a>
+                  </Link>
+                ))}
+              </section>
             </div>
           </div>
         </header>
