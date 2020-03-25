03e94c9604360aee5f8d83ca12c464f5aac7e37c
AlexTicovschi <alexmticovschi@gmail.com>
2020-03-25 16:38:40 +0000

    updating blogs - showing checked categories and tags

--- frontend/components/crud/UpdateBlog/UpdateBlog.js
+++ frontend/components/crud/UpdateBlog/UpdateBlog.js
@@ -13,6 +13,10 @@ const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
 
 const UpdateBlog = ({ router }) => {
   const [body, setBody] = useState('');
+  const [categories, setCategories] = useState([]);
+  const [tags, setTags] = useState([]);
+  const [checkedCategories, setCheckedCategories] = useState([]);
+  const [checkedTags, setCheckedTags] = useState([]);
   const [values, setValues] = useState({
     error: '',
     success: '',
@@ -24,20 +28,67 @@ const UpdateBlog = ({ router }) => {
   useEffect(() => {
     setValues({ ...values, formData: new FormData() });
     initBlog();
+    initCategories();
+    initTags();
+
+    console.log(checkedCategories, checkedTags);
   }, [router]);
 
   const { error, success, formData, title } = values;
   const { slug } = router.query;
 
   const initBlog = async () => {
-    let blog;
     try {
-      blog = await fetchBlog(slug);
+      let blog = await fetchBlog(slug);
       if (blog) {
         setValues({ ...values, title: blog.title });
         setBody(blog.body);
+        setCategoriesArray(blog.categories);
+        setTagsArray(blog.tags);
       }
     } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const setCategoriesArray = categories => {
+    let categoriesArray = [];
+    categories.map(category => categoriesArray.push(category._id));
+    setCheckedCategories(categoriesArray);
+  };
+
+  const setTagsArray = tags => {
+    let tagsArray = [];
+    tags.map(tag => tagsArray.push(tag._id));
+    setCheckedTags(tagsArray);
+  };
+
+  const checkCategoriesAndTags = (arr, id) => {
+    return arr.indexOf(id) === -1 ? false : true;
+  };
+
+  // initialize categories state
+  const initCategories = async () => {
+    try {
+      let categories = await getCategories();
+      if (categories) {
+        setCategories(categories);
+      }
+    } catch (error) {
+      setValues({ ...values, error: categories.error });
+      console.error(error);
+    }
+  };
+
+  // initialize tags state
+  const initTags = async () => {
+    try {
+      let tags = await getTags();
+      if (tags) {
+        setTags(tags);
+      }
+    } catch (error) {
+      setValues({ ...values, error: tags.error });
       console.error(error);
     }
   };
@@ -47,6 +98,52 @@ const UpdateBlog = ({ router }) => {
     // form data to be processed by the backend to create a new blog
     formData.set(name, value);
     setValues({ ...values, [name]: value, formData, error: '' });
+  };
+
+  // add or remove checked categories from state
+  const handleCategoryToggleCheckbox = categoryId => () => {
+    setValues({ ...values, error: '' });
+
+    const allCheckedCategories = [...checkedCategories];
+
+    // get the index of current checked category
+    const checkedCategory = checkedCategories.indexOf(categoryId);
+
+    // if the current checked category is not in the state, add it
+    // else remove the category from the state
+    if (checkedCategory === -1) {
+      allCheckedCategories.push(categoryId);
+    } else {
+      allCheckedCategories.splice(checkedCategory, 1);
+    }
+
+    setCheckedCategories(allCheckedCategories);
+    formData.set('categories', allCheckedCategories);
+
+    console.log(allCheckedCategories);
+  };
+
+  // add or remove checked tags from state
+  const handleTagToggleCheckbox = tagId => () => {
+    setValues({ ...values, error: '' });
+
+    const allCheckedTags = [...checkedTags];
+
+    // get the index of current checked tag
+    const checkedTag = checkedTags.indexOf(tagId);
+
+    // if the current checked tag is not in the state, add it
+    // else remove the tag from the state
+    if (checkedTag === -1) {
+      allCheckedTags.push(tagId);
+    } else {
+      allCheckedTags.splice(checkedTag, 1);
+    }
+
+    setCheckedTags(allCheckedTags);
+    formData.set('tags', allCheckedTags);
+
+    console.log(allCheckedTags);
   };
 
   const handleBody = event => {
@@ -105,17 +202,21 @@ const UpdateBlog = ({ router }) => {
             className='list-unstyled'
             style={{ maxHeight: '120px', overflowY: 'scroll' }}
           >
-            {/* {categories &&
+            {categories &&
               categories.map(category => (
                 <li key={category._id}>
                   <input
+                    checked={checkCategoriesAndTags(
+                      checkedCategories,
+                      category._id
+                    )}
                     onChange={handleCategoryToggleCheckbox(category._id)}
                     type='checkbox'
                     className='mr-2'
                   />
                   <label className='form-check-label'>{category.name}</label>
                 </li>
-              ))} */}
+              ))}
           </ul>
 
           <hr />
@@ -124,17 +225,18 @@ const UpdateBlog = ({ router }) => {
             className='list-unstyled'
             style={{ maxHeight: '120px', overflowY: 'scroll' }}
           >
-            {/* {tags &&
+            {tags &&
               tags.map(tag => (
                 <li key={tag._id}>
                   <input
+                    checked={checkCategoriesAndTags(checkedTags, tag._id)}
                     onChange={handleTagToggleCheckbox(tag._id)}
                     type='checkbox'
                     className='mr-2'
                   />
                   <label className='form-check-label'>{tag.name}</label>
                 </li>
-              ))} */}
+              ))}
           </ul>
         </div>
       </div>
