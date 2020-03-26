9c8d77021dfe32fc855e85bfa2daa71b69837e83
AlexTicovschi <alexmticovschi@gmail.com>
2020-03-26 20:32:15 +0000

    adding SEO for categories and tags

--- frontend/pages/categories/[slug].js
+++ frontend/pages/categories/[slug].js
@@ -7,29 +7,60 @@ import renderHTML from 'react-render-html';
 import moment from 'moment';
 import Card from '../../components/blog/Card/Card';
 
-const Category = ({ category, blogs }) => {
+const Category = ({ category, blogs, query }) => {
   return (
-    <React.Fragment>
-      <Layout>
-        <main>
-          <div className='container text-center'>
-            <header>
-              <div className='col-xl-12'>
-                <h1 className='display-4 font-weight-bold py-5'>
-                  {category.name}
-                </h1>
-                {blogs.map((b, i) => (
-                  <div>
-                    <Card key={i} blog={b} />
-                    <hr />
-                  </div>
-                ))}
-              </div>
-            </header>
-          </div>
-        </main>
-      </Layout>
-    </React.Fragment>
+    <Layout>
+      <Head>
+        <title>
+          {category.name} | {APP_NAME}
+        </title>
+        <meta
+          name='description'
+          content={`Best programming blogs and tutorials on ${category.name}`}
+        />
+
+        <link rel='canonical' href={`${DOMAIN}/categories/${query.slug}`} />
+        <meta property='og:title' content={`${category.name} | ${APP_NAME}`} />
+        <meta
+          property='og:description'
+          content={`${category.name} | ${APP_NAME}`}
+        />
+        <meta property='og:type' content='website' />
+        <meta
+          property='og:url'
+          content={`${DOMAIN}/categories/${query.slug}`}
+        />
+        <meta property='og:site_name' content={`${APP_NAME}`} />
+
+        <meta
+          property='og:image'
+          content={`${DOMAIN}/images/bloggingcoder.jpg`}
+        />
+        <meta
+          property='og:image:secure_url'
+          content={`${DOMAIN}/images/bloggingcoder.jpg`}
+        />
+        <meta property='og:image:type' content='/image/jpg' />
+        <meta property='fb:app_id' content={`${FB_APP_ID}`} />
+      </Head>
+      <main>
+        <div className='container text-center'>
+          <header>
+            <div className='col-xl-12'>
+              <h1 className='display-4 font-weight-bold py-5'>
+                {category.name}
+              </h1>
+              {blogs.map(blog => (
+                <div>
+                  <Card key={blog._id} blog={blog} />
+                  <hr />
+                </div>
+              ))}
+            </div>
+          </header>
+        </div>
+      </main>
+    </Layout>
   );
 };
 
@@ -39,7 +70,7 @@ Category.getInitialProps = async ({ query }) => {
 
   try {
     if (data) {
-      return { category: data.category, blogs: data.blogs };
+      return { category: data.category, blogs: data.blogs, query };
     }
   } catch (error) {
     console.error(error);
--- frontend/pages/tags/[slug].js
+++ frontend/pages/tags/[slug].js
@@ -7,27 +7,55 @@ import renderHTML from 'react-render-html';
 import moment from 'moment';
 import Card from '../../components/blog/Card/Card';
 
-const Tag = ({ tag, blogs }) => {
+const Tag = ({ tag, blogs, query }) => {
   return (
-    <React.Fragment>
-      <Layout>
-        <main>
-          <div className='container text-center'>
-            <header>
-              <div className='col-xl-12'>
-                <h1 className='display-4 font-weight-bold py-5'>{tag.name}</h1>
-                {blogs.map(blog => (
-                  <div>
-                    <Card key={blog._id} blog={blog} />
-                    <hr />
-                  </div>
-                ))}
-              </div>
-            </header>
-          </div>
-        </main>
-      </Layout>
-    </React.Fragment>
+    <Layout>
+      <Head>
+        <title>
+          {tag.name} | {APP_NAME}
+        </title>
+        <meta
+          name='description'
+          content={`Best programming blogs and tutorials on ${tag.name}`}
+        />
+
+        <link rel='canonical' href={`${DOMAIN}/categories/${query.slug}`} />
+        <meta property='og:title' content={`${tag.name} | ${APP_NAME}`} />
+        <meta property='og:description' content={`${tag.name} | ${APP_NAME}`} />
+        <meta property='og:type' content='website' />
+        <meta
+          property='og:url'
+          content={`${DOMAIN}/categories/${query.slug}`}
+        />
+        <meta property='og:site_name' content={`${APP_NAME}`} />
+
+        <meta
+          property='og:image'
+          content={`${DOMAIN}/images/bloggingcoder.jpg`}
+        />
+        <meta
+          property='og:image:secure_url'
+          content={`${DOMAIN}/images/bloggingcoder.jpg`}
+        />
+        <meta property='og:image:type' content='/image/jpg' />
+        <meta property='fb:app_id' content={`${FB_APP_ID}`} />
+      </Head>
+      <main>
+        <div className='container text-center'>
+          <header>
+            <div className='col-xl-12'>
+              <h1 className='display-4 font-weight-bold py-5'>{tag.name}</h1>
+              {blogs.map(blog => (
+                <div>
+                  <Card key={blog._id} blog={blog} />
+                  <hr />
+                </div>
+              ))}
+            </div>
+          </header>
+        </div>
+      </main>
+    </Layout>
   );
 };
 
@@ -37,7 +65,7 @@ Tag.getInitialProps = async ({ query }) => {
 
   try {
     if (data) {
-      return { tag: data.tag, blogs: data.blogs };
+      return { tag: data.tag, blogs: data.blogs, query };
     }
   } catch (error) {
     console.error(error);
