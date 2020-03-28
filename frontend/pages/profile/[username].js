42d9cd798cacfebd57d061e69a2d01db9b1f420d
AlexTicovschi <alexmticovschi@gmail.com>
2020-03-28 10:02:36 +0000

    adding SEO to profile page

--- frontend/pages/profile/[username].js
+++ frontend/pages/profile/[username].js
@@ -5,58 +5,83 @@ import moment from 'moment';
 import { userProfile } from '../../actions/user';
 import { API, DOMAIN, APP_NAME, FB_APP_ID } from '../../config';
 
-const UserProfile = ({ user, blogs }) => {
+const UserProfile = ({ user, blogs, query }) => {
+  console.log(user);
   return (
-    <>
-      <Layout>
-        <div className='container'>
-          <div className='row'>
-            <div className='col-xl-12'>
-              <div className='card'>
-                <div className='card-body'>
-                  <h5>{user.name}</h5>
-                  <p className='text-muted'>
-                    Joined {moment(user.createdAt).fromNow()}
-                  </p>
-                  {/* {JSON.stringify(blogs)} */}
-                </div>
+    <Layout>
+      <Head>
+        <title>
+          {user.name} | {APP_NAME}
+        </title>
+        <meta name='description' content={`Blogs by ${user.username}`} />
+
+        <link rel='canonical' href={`${DOMAIN}/profile/${query.username}`} />
+        <meta property='og:title' content={`${user.username} | ${APP_NAME}`} />
+        <meta property='og:description' content={`Blogs by ${user.username}`} />
+        <meta property='og:type' content='website' />
+        <meta
+          property='og:url'
+          content={`${DOMAIN}/profile/${query.username}`}
+        />
+        <meta property='og:site_name' content={`${APP_NAME}`} />
+
+        <meta
+          property='og:image'
+          content={`${DOMAIN}/images/bloggingcoder.jpg`}
+        />
+        <meta
+          property='og:image:secure_url'
+          content={`${DOMAIN}/images/bloggingcoder.jpg`}
+        />
+        <meta property='og:image:type' content='/image/jpg' />
+        <meta property='fb:app_id' content={`${FB_APP_ID}`} />
+      </Head>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-xl-12'>
+            <div className='card'>
+              <div className='card-body'>
+                <h5>{user.name}</h5>
+                <p className='text-muted'>
+                  Joined {moment(user.createdAt).fromNow()}
+                </p>
               </div>
             </div>
           </div>
         </div>
-        <div className='container mt-5'>
-          <div className='row'>
-            <div className='col-md-6'>
-              <div className='card'>
-                <div className='card-body'>
-                  <div className='p-3 mb-2 bg-info text-white'>{`Latest blogs by ${user.name}`}</div>
-                  {blogs.map(blog => (
-                    <div className='py-2' key={blog._id}>
-                      <Link href={`/blogs/${blog.slug}`}>
-                        <a className='lead'>{blog.title}</a>
-                      </Link>
-                    </div>
-                  ))}
-                </div>
+      </div>
+      <div className='container mt-5'>
+        <div className='row'>
+          <div className='col-md-6'>
+            <div className='card'>
+              <div className='card-body'>
+                <div className='p-3 mb-2 bg-info text-white'>{`Latest blogs by ${user.name}`}</div>
+                {blogs.map(blog => (
+                  <div className='py-2' key={blog._id}>
+                    <Link href={`/blogs/${blog.slug}`}>
+                      <a className='lead'>{blog.title}</a>
+                    </Link>
+                  </div>
+                ))}
               </div>
             </div>
-            <div className='col-md-6'>
-              <div className='card'>
-                <div className='card-body'>
-                  <div className='p-3 mb-2 bg-info text-white'>{`Message ${user.name}`}</div>
-                </div>
+          </div>
+          <div className='col-md-6'>
+            <div className='card'>
+              <div className='card-body'>
+                <div className='p-3 mb-2 bg-info text-white'>{`Message ${user.name}`}</div>
               </div>
             </div>
           </div>
         </div>
-      </Layout>
-    </>
+      </div>
+    </Layout>
   );
 };
 
-UserProfile.getInitialProps = async ({ query: { username } }) => {
-  const response = await userProfile(username);
-  return { user: response.user, blogs: response.blogs };
+UserProfile.getInitialProps = async ({ query }) => {
+  const response = await userProfile(query.username);
+  return { user: response.user, blogs: response.blogs, query };
 };
 
 export default UserProfile;
