34eb3f7b9df514a06907ba656f557dac7e8250d4
AlexTicovschi <alexmticovschi@gmail.com>
2020-04-03 14:47:36 +0100

    reset password implementation on the frontend

--- frontend/pages/auth/password/reset/[token].js
+++ frontend/pages/auth/password/reset/[token].js
@@ -1,21 +1,22 @@
 import { useState } from 'react';
-import Layout from '../../../components/Layout';
-import { forgotPassword } from '../../../actions/auth';
+import Layout from '../../../../components/Layout';
+import { withRouter } from 'next/router';
+import Link from 'next/link';
+import { resetPassword } from '../../../../actions/auth';
 
-const ForgotPassword = () => {
+const ResetPassword = ({ router }) => {
   const [values, setValues] = useState({
-    email: '',
+    name: '',
+    newPassword: '',
     message: '',
-    error: '',
-    showForm: true
+    error: ''
   });
 
-  const { email, message, error, showForm } = values;
+  const { name, newPassword, message, error } = values;
 
   const handleInputChange = name => event => {
     setValues({
       ...values,
-      message: '',
       error: '',
       [name]: event.target.value
     });
@@ -26,15 +27,21 @@ const ForgotPassword = () => {
   const handleSubmit = async e => {
     e.preventDefault();
     setValues({ ...values, message: '', error: '' });
-    const data = await forgotPassword({ email });
+    const data = await resetPassword({
+      newPassword,
+      resetPasswordLink: router.query.token
+    });
     if (data.error) {
-      setValues({ ...values, error: data.error });
+      setValues({
+        ...values,
+        error: data.error,
+        newPassword: ''
+      });
     } else {
       setValues({
         ...values,
         message: data.message,
-        email: '',
-        showForm: false
+        error: false
       });
     }
   };
@@ -43,7 +50,16 @@ const ForgotPassword = () => {
     message ? <div className='alert alert-success'>{message}</div> : '';
 
   const showErrorMessage = () =>
-    error ? <div className='alert alert-danger'>{error}</div> : '';
+    error ? (
+      <div className='alert alert-danger'>
+        {error}{' '}
+        <Link href='/auth/password/forgot'>
+          <a>Forgot Password?</a>
+        </Link>
+      </div>
+    ) : (
+      ''
+    );
 
   return (
     <Layout>
@@ -56,24 +72,22 @@ const ForgotPassword = () => {
         </div>
         <div className='row'>
           <div className='col-xl-6 col-md-8 mx-auto pt-5'>
-            <h3 className='text-center pb-3'>Forgot Password</h3>
-            <p className='text-center pb-3'>
-              Please enter your email address to request a pasword reset.
-            </p>
+            <h3 className='text-center pb-3'>Reset Password</h3>
+            <p className='text-center pb-3'>Please enter your new password.</p>
             <form onSubmit={handleSubmit}>
               <div className='form-group pt-3'>
                 <input
-                  type='email'
-                  onChange={handleInputChange('email')}
+                  type='password'
+                  onChange={handleInputChange('newPassword')}
                   className='form-control'
-                  value={email}
-                  placeholder='Enter your email'
+                  value={newPassword}
+                  placeholder='Enter your new password'
                   required
                 />
               </div>
               <div>
                 <button type='submit' className='btn btn-success btn-block'>
-                  Request Password Reset
+                  Reset Password
                 </button>
               </div>
             </form>
@@ -84,4 +98,4 @@ const ForgotPassword = () => {
   );
 };
 
-export default ForgotPassword;
+export default withRouter(ResetPassword);
