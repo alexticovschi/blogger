c3367390211fb22cc02af6aed085d53f54167715
Alex Ticovschi <alexmticovschi@gmail.com>
2020-09-22 10:45:26 +0100

    adding favicon

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,21 +1,22 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang='en'>
+      <Html lang="en">
         <Head>
-          <meta charSet='UTF-8' />
+          <meta charSet="UTF-8" />
           <meta
-            name='viewport'
-            content='width=device-width, initial-scale=1.0'
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
           />
-          <link rel='stylesheet' href='/css/nprogress.css' />
-        </Head>
+          <link rel="shortcut icon" href="favicon.ico" />
+          <link rel="stylesheet" href="/css/nprogress.css" />
+        </Head>{" "}
         <body>
           <Main />
           <NextScript />
-        </body>
+        </body>{" "}
       </Html>
     );
   }
